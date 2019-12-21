c860ff3a431e2ccd4f289a206da574289e411b19
sujanghadi <58872614+sujanghadi@users.noreply.github.com>
2019-12-21 22:45:14 +0530

    Add files via upload
    In Python, Strings are arrays of bytes representing Unicode characters. However, Python does not have a character data type, a single character is simply a string with a length of 1. Square brackets can be used to access elements of the string.

---
+++ string.py
@@ -0,0 +1,11 @@
+string="this is string"
+print(string)
+print("this also work")
+
+print('''this is multiline 
+	code
+	hhhk
+	khkkhkh''')
+print("first char of string is:",string[1:5])
+print('I\'am "sujan"')
+print("{} {} {}".format("sujan","kiran","are brothers"))
