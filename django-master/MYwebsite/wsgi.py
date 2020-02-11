d4dad1b9f66bbfbd70ebe04c1ef48653a12944f1
sujanghadi <58872614+sujanghadi@users.noreply.github.com>
2020-02-11 12:04:15 +0530

    Add files via upload

---
+++ django-master/MYwebsite/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for MYwebsite project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MYwebsite.settings')
+
+application = get_wsgi_application()
